2366835f67a7124334cab361ee6026c9a66f5434
Suraj <surajparkale16@gmail.com>
2021-05-25 12:34:40 +0530

    Ternary If-Else Code

---
+++ c++/ternaryIf_Else.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int marks = 46;
+    string result = (marks >= 40) ? "Passed" : "Failed";   Code to cheak pass or fail
+
+    cout<<result<<endl;
+    return 0;
+}
